df1bfec99a3222200374472bb8d5b6821cf5265e
rezafikkri <fikkri.reza@gmail.com>
2024-11-01 13:30:11 +0700

    feat: create google form feedback

--- src/renderer/src/components/Footer.jsx
+++ src/renderer/src/components/Footer.jsx
@@ -4,11 +4,13 @@ import { translate } from '../../../main/utils';
 
 export default function Footer() {
   const {locale, resources} = useContext(LocaleContext);
+  let feedbackLink = 'https://forms.gle/MWtbZcQ3AWDxHTwH9';
+  if (locale === 'en') feedbackLink = 'https://forms.gle/zS2UprPyfcD92j4j9';
 
   return (
     <footer className="p-4 text-white/80 [&_a]:text-indigo-400 [&_a]:font-medium text-xs relative z-10">
       {translate(locale, 'footer.sendFeedbackText', resources)}
-      <a href="https://forms.gle/zS2UprPyfcD92j4j9" target="_blank" className="inline-block hover:text-indigo-300">{translate(locale, 'footer.sendFeedbackLinkText', resources)}</a>
+      <a href={feedbackLink} target="_blank" className="inline-block hover:text-indigo-300">{translate(locale, 'footer.sendFeedbackLinkText', resources)}</a>
     </footer>
   );
 }
