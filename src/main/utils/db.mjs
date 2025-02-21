9650f13e9b00f9668dc8e43ac595a43c108c941e
rezafikkri <fikkri.reza@gmail.com>
2025-02-22 06:37:59 +0700

    feat: create prepareRelease function for automatic genereate sKeyLicense and releasedAt

--- src/main/utils/db.mjs
+++ src/main/utils/db.mjs
@@ -1,6 +1,8 @@
-import { Pool } from 'pg';
+import pg from 'pg';
+const { Pool } = pg;
+
 const connectionString = 'postgresql://postgres.hyqaaakuutzfjvyphlqe:uSujKe1D6EQvkpVo@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres';
- 
+
 const pool = new Pool({
   connectionString,
 });
