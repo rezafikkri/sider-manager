ab6f39d65b3b6857ac340b176ed7bf1efa343fe6
rezafikkri <fikkri.reza@gmail.com>
2025-02-23 07:47:50 +0700

    feat: add released at data to check-update.json file

--- src/main/services/check-update.js
+++ src/main/services/check-update.js
@@ -35,20 +35,24 @@ async function refreshAT() {
   writeFileSync(path.join(getSettingsPath(), 'gd-at.json'), JSON.stringify(newAccessToken));
 }
 
-async function getFolders() {
-  const { at: accessToken } = await loadAccessToken();
-  try {
-    const url = 'https://www.googleapis.com/drive/v3/files?orderBy=name_natural%20desc&q=%27upload-sider-manager%40caramel-vim-451414-b6.iam.gserviceaccount.com%27%20in%20owners%20and%20mimeType%20%3D%20%27application%2Fvnd.google-apps.folder%27';
-    const res = await needle('get', url, {
-      headers: { 'Authorization': `Bearer ${accessToken}` }
-    });
-    if (res.statusCode === 401) {
-      refreshAT(rt);
-      throw new UnauthorizeError(JSON.stringify(res.body));
-    }
-    if (res.statusCode !== 200) throw new Error(JSON.stringify(res.body));
+async function get(url, accessToken) {
+  const res = await needle('get', url, {
+    headers: { 'Authorization': `Bearer ${accessToken}` }
+  });
+  if (res.statusCode === 401) {
+    refreshAT(rt);
+    throw new UnauthorizeError(JSON.stringify(res.body));
+  }
+  if (res.statusCode !== 200) throw new Error(JSON.stringify(res.body));
+  return res.body;
+}
 
-    return res.body.files;
+async function getFolders() {
+  try {
+    const { at: accessToken } = await loadAccessToken();
+    const url = 'https://www.googleapis.com/drive/v3/files?orderBy=name_natural%20desc&q=%27upload-sider-manager%40caramel-vim-451414-b6.iam.gserviceaccount.com%27%20in%20owners%20and%20mimeType%20%3D%20%27application%2Fvnd.google-apps.folder%27';
+    const body = await get(url, accessToken);
+    return body.files;
   } catch (err) {
     if (!(err instanceof UnauthorizeError)) {
       log.error(generateErrorLogMessage(
@@ -88,13 +92,36 @@ function checkSmallerThanVersion(currentVersion, latestVersion) {
   return false;
 }
 
+async function getReleasedAt(folderId) {
+  try {
+    const { at: accessToken } = await loadAccessToken();
+    const urlGetFileId = `https://www.googleapis.com/drive/v3/files?q=name%3D%27released-at.txt%27%20and%20%27${folderId}%27%20in%20parents`;
+    const bodyFileId = await get(urlGetFileId, accessToken);
+    const fileId = bodyFileId.files[0].id;
+
+    // get content
+    const urlGetContent = `https://www.googleapis.com/drive/v3/files/${fileId}?alt=media`;
+    const bodyContent = await get(urlGetContent, accessToken);
+    return parseInt(bodyContent);
+  } catch (err) {
+    if (!(err instanceof UnauthorizeError)) {
+      log.error(generateErrorLogMessage(
+        app.getVersion(),
+        os.version(),
+        process.versions.electron,
+        err.stack,
+      ));
+    }
+    return false;
+  }
+}
+
 export default async function checkUpdate() {
   // check if native notification is support or not
   if (!Notification.isSupported()) return false;
 
   const { locale } = getSettings();
   const localeResources = getLocaleResources();
-
   const currentAppVersion = app.getVersion();
 
   const tempDirPath = path.join(app.getPath('temp'), 'sider-manager');
@@ -124,9 +151,14 @@ export default async function checkUpdate() {
         body: translate(locale, 'notification.body', localeResources, latestReleaseVersion),
       }).show();
 
+      // get released-at.txt content
+      const releasedAt = await getReleasedAt(versions[0].id);
+      if (!releasedAt) return false;
+
       writeFileSync(checkUpdateFilePath, JSON.stringify({
         version: latestReleaseVersion,
         limitTime: Math.floor(new Date().getTime() / 1000) + (3600 * 24 * 2), // 2 days
+        releasedAt,
       }));
 
       return true;
@@ -147,9 +179,14 @@ export default async function checkUpdate() {
       body: translate(locale, 'notification.body', localeResources, latestReleaseVersion),
     }).show();
 
+    // get released-at.txt content
+    const releasedAt = await getReleasedAt(versions[0].id);
+    if (!releasedAt) return false;
+
     writeFileSync(checkUpdateFilePath, JSON.stringify({
       version: latestReleaseVersion,
       limitTime: Math.floor(new Date().getTime() / 1000) + (3600 * 24 * 2), // 2 days
+      releasedAt,
     }));
   }
 }
--- src/main/utils/create-released-at-file.mjs
+++ src/main/utils/create-released-at-file.mjs
@@ -1,7 +1,7 @@
 import { writeFileSync } from 'node:fs';
 import path from 'node:path';
 
-const releasedAt = 1740247817;
+const releasedAt = 1740271583;
 
 function createReleasedAtFile() {
   writeFileSync(path.join('release', 'released-at.txt'), releasedAt.toString());
