5f5ad4817323c8c4d40b6e0e521ff935ab9d199f
rezafikkri <fikkri.reza@gmail.com>
2025-02-28 16:36:36 +0700

    build: remove pnpm cache in build workflow

--- src/main/utils/create-released-at-file.mjs
+++ src/main/utils/create-released-at-file.mjs
@@ -1,7 +1,7 @@
 import { writeFileSync } from 'node:fs';
 import path from 'node:path';
 
-const releasedAt = 1740733499;
+const releasedAt = 1740735324;
 
 function createReleasedAtFile() {
   writeFileSync(path.join('release', 'released-at.txt'), releasedAt.toString());
